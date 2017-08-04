b2c393ce1afdf239be2cbc057261610c54dd11c4
Kristóf <illeskristof1994@gmail.com>
2017-08-04 12:55:52 +0200

    refactoring1

--- bowling.py
+++ bowling.py
@@ -7,8 +7,6 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
@@ -30,14 +28,11 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
